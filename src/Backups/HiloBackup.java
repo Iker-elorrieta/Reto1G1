81c12f873469dab63ab066d2f7498a3276caf01e
jihankachouraa <jihan.kachouraa@elorrieta-errekmari.com>
2025-10-23 12:05:01 +0200

    Arreglar backups exportando jar Co-authored-by: danielalonsomendez <danielalonso308@gmail.com>

--- src/Backups/HiloBackup.java
+++ src/Backups/HiloBackup.java
@@ -9,26 +9,34 @@ import javax.swing.JLabel;
 public class HiloBackup extends Thread {
 
 	JLabel label;
-    public HiloBackup(JLabel label) {
+
+	public HiloBackup(JLabel label) {
 		this.label = label;
-    }
-    
-    @Override
-    public void run() {
-    	try {
-			ProcessBuilder pb = new ProcessBuilder("java", "Backups.ProcesoBackup");
-			pb.directory(new File("target/classes"));
+	}
+
+	@Override
+	public void run() {
+		try {
+			// En proceso...
+			label.setText("Backup en proceso...");
+			label.setForeground(Color.BLUE);
+			
+			// Inicio proceso
+			ProcessBuilder pb = new ProcessBuilder("java", "-jar", "backups.jar");
+			pb.directory(new File("."));
 			Process proces = pb.start();
 			int exitCode = proces.waitFor();
-			label.setText("Backup finalizado correctamente. ("+ exitCode+")");
+
+			// Finalizado
+			label.setForeground(Color.GREEN);
+			label.setText("Backup finalizado correctamente (" + exitCode + ")");
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 			label.setText("Backup finalizado con error.");
-			label.setForeground(Color.RED);	
+			label.setForeground(Color.RED);
 
 		}
 
-    }
-
+	}
 
 }
