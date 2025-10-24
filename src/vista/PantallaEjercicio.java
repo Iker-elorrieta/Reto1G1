b43b5a0ce6e6f1b0d2f028276539fb288652104d
izarovega <izaro.santiagovei@elorrieta-errekamari.com>
2025-10-24 10:52:23 +0200

    Quitar imports no usados

--- src/vista/PantallaEjercicio.java
+++ src/vista/PantallaEjercicio.java
@@ -1,6 +1,5 @@
 package vista;
 
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,7 +12,6 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.event.ActionEvent;
-import javax.swing.border.LineBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Graphics;
